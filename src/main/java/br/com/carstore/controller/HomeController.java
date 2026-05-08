31803d89f263e9407e1dc8512edac517f7b5c3fa
luccas.vannucci <luccas.vannucci004@gmail.com>
2026-05-07 21:57:33 -0300

    Atualização do Projeto Spring implemantando a segurança da pagina web

--- src/main/java/br/com/carstore/controller/HomeController.java
+++ src/main/java/br/com/carstore/controller/HomeController.java
@@ -23,61 +23,22 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("carDTO", new CarDTO());
-        return "index";
-    }
-
-    @PostMapping("/cars")
-    public String createCar(CarDTO carDTO, BindingResult result) {
-
-        if (carDTO.getId() != null && !carDTO.getId().isBlank()) {
-
-            service.update(carDTO.getId(), carDTO);
-
-        } else {
-
-            service.save(carDTO);
-
-        }
+    public String index() {
 
         return "redirect:/cars";
-
     }
 
     @GetMapping("/cars")
     public String getCars(Model model) {
+
         List<CarDTO> allCars = service.findAll();
+
         model.addAttribute("cars", allCars);
-        return "dashboard";
+
+        return "public/dashboard";
     }
 
-    @GetMapping("/cars/edit")
-    public String editCar(@RequestParam("id") String id, Model model) {
 
-        CarDTO car = service.findById(id);
 
-        if (car == null) {
-
-            return "redirect:/cars";
-
-        }
-        model.addAttribute("carDTO", car);
-
-        return "index";
-
-    }
-
-    @PostMapping("/cars/delete")
-    public String deleteCar(@RequestParam("id") String id, Model model) {
-
-        service.deleteById(id);
-
-        List<CarDTO> cars = service.findAll();
-        model.addAttribute("cars", cars);
-
-        return "redirect:/cars";
-
-    }
 
 }
